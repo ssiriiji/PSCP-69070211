1d0c11c2dc80437f5db82c97293107ddf7c08aa0
ssiriiji <136876864+ssiriiji@users.noreply.github.com>
2026-07-24 01:32:19 +0700

    add file submition.md

--- Learning_Logs/3017/Bill.py
+++ Learning_Logs/3017/Bill.py
@@ -1,20 +1,21 @@
-"""this is funct"""
+"""this is main"""
 def main():
     """this is code"""
     money = int(input())
-    if money < 500:
-        key = 50
-        key2 = key+money
-        result = key2+(key2*7/100)
-        print(f"{result:.2f}")
-    elif money > 10000:
-        key = 1000
-        key2 = key+money
-        result = key2+(key2*7/100)
-        print(f"{result:.2f}")
-    elif 500 <= money <= 10000:
-        key = money*(10/100)
-        key2 = key+money
-        result = key2+(key2*7/100)
-        print(f"{result:.2f}")
+
+    if money <= 500:
+        service = money + 50
+        vat = service * 7/100
+        sum_money = service + vat
+        print(f"{sum_money:.2f}")
+    elif 500 < money < 10000:
+        service = money + (money * 10/100)
+        vat = service * 7/100
+        sum_money = service + vat
+        print(f"{sum_money:.2f}")
+    else:
+        service = money + 1000
+        vat = service * 7/100
+        sum_money = service + vat
+        print(f"{sum_money:.2f}")
 main()
